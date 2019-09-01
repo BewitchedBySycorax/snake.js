da5ef4a49105188eee94070537818af862a39c37
Alexey Voytovsky <al.voytovsky@gmail.com>
2019-09-02 00:04:51 +0300

    'createBoard' method is being implemented.

--- js/snake.js
+++ js/snake.js
@@ -3,8 +3,22 @@ function random(min, max) {
 };
 
 var game = {
+    size: 20,
     createBoard: function() {
         console.log('create board');
+        var table = document.createElement('table');
+
+        for ( var i = 0; i < this.size; i++ ) {
+            var tr = document.createElement('tr');
+
+            for ( var j = 0; j < this.size; j++ ){
+                var td = document.createElement('td');
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+        
+        document.getElementById('snake-field').appendChild(table);
     },
     createSnake: function() {
         console.log('create snake');
